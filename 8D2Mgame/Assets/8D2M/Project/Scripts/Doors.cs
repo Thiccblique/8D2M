25ee552780459241445c0244761e6045c19c2fcc
Thiccblique <104588992+Thiccblique@users.noreply.github.com>
2023-06-12 18:49:00 -0700

    added doors and cross hair
    So I pretty much added a reach system to interact with objects, this allows me to open doors

---
+++ 8D2Mgame/Assets/8D2M/Project/Scripts/Doors.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Doors : MonoBehaviour
+{
+
+    [Header("Keybinds")]
+    public KeyCode Interact = KeyCode.E;
+
+    public Animator door;
+    public GameObject openText;
+    public AudioSource doorSound;
+    public bool inReach;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inReach = false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        // If we look at the object, text will apear 
+        // Tag reach applys to the "Reach" object that is conected to the player cam, substituting arrays
+        if (other.gameObject.tag == "Reach")
+        {
+            inReach = true;
+            openText.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        // If we look away from the object, text will disapear
+        if (other.gameObject.tag == "Reach")
+        {
+            inReach = false;
+            openText.SetActive(false);
+        }
+    }
+  
+    // Update is called once per frame
+    void Update()
+    {
+        // If the door is closed, the open animation plays, if not the closed animation plays
+        if (inReach && Input.GetKeyDown(Interact))
+        {
+            DoorOpens();
+        }
+        else
+        {
+            DoorCloses();
+        }
+    }
+   
+    // If ran, it runs the "open" animation, and ignorse the closed animation 
+    void DoorOpens()
+    {
+        //Debug.Log("It Opens");
+        door.SetBool("DoorOpen", true);
+        door.SetBool("DoorClosed", false);
+        doorSound.Play();
+
+    }
+    // If ran, it runs the "closed" animation, and ignorse the open animation 
+    void DoorCloses()
+    {
+        //Debug.Log("It Closes");
+        door.SetBool("DoorOpen", false);
+        door.SetBool("DoorClosed", true);
+    }
+}
