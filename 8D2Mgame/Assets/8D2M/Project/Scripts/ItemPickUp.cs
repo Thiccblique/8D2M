8d908745a7635740d68683e0badb21c9b103b92a
Thiccblique <104588992+Thiccblique@users.noreply.github.com>
2023-06-13 18:57:02 -0700

    added an item pickup system where you can pick up and drop items
    Also gave items audio for pickup

---
+++ 8D2Mgame/Assets/8D2M/Project/Scripts/ItemPickUp.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickUp : MonoBehaviour
+{
+
+    [Header("Keybinds")]
+    public KeyCode Grab = KeyCode.E;
+    public KeyCode Drop = KeyCode.Q;
+
+    [Header("Objects")]
+    public GameObject Item;
+    public Transform ItemHolder;
+
+    public GameObject openText;
+    public AudioSource pickUpSound;
+    public bool inReach;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Stops item from having gravity.
+        Item.GetComponent<Rigidbody>().isKinematic = false;
+        inReach = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(Drop))
+        {
+            DropItem();
+        }
+    }
+
+    private void DropItem()
+    {
+        //Drops item
+        ItemHolder.DetachChildren();
+        // Gives item new possition when droppped 
+        Item.transform.eulerAngles = new Vector3(Item.transform.position.x, Item.transform.position.z, Item.transform.position.y);
+        // re-enables gravity to allow item fall drop
+        Item.GetComponent<Rigidbody>().isKinematic = false;
+        Item.GetComponent<MeshCollider>().enabled = true;
+    }
+
+    private void GrabItem()
+    {
+        Item.GetComponent<Rigidbody>().isKinematic = true;
+
+        Item.transform.position = ItemHolder.transform.position;
+        Item.transform.rotation = ItemHolder.transform.rotation;
+
+        Item.GetComponent<MeshCollider>().enabled = false;
+
+        Item.transform.SetParent(ItemHolder);
+        pickUpSound.Play();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Reach")
+        {
+            if (inReach && Input.GetKey(Grab))
+            {
+                GrabItem();
+            }
+        }
+    }
+   
+    void OnTriggerEnter(Collider other)
+    {
+        // If we look at the object, text will apear 
+        // Tag reach applys to the "Reach" object that is conected to the player cam, substituting arrays
+        if (other.gameObject.tag == "Reach")
+        {
+            inReach = true;
+            openText.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        // If we look away from the object, text will disapear
+        if (other.gameObject.tag == "Reach")
+        {
+            inReach = false;
+            openText.SetActive(false);
+        }
+    }
+}
